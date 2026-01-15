06b0efe97d6644425058311bb6bd721704444d9b
ANTOINE METHOT <amethot@etu.uqac.ca>
2026-01-15 08:51:07 -0500

    feat: acceuil template with dynamic content

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-from flask import Flask,send_from_directory
+from flask import Flask, send_from_directory, render_template, request
 
 app = Flask(__name__)
 
 # Route principale
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def accueil():
-    return send_from_directory("static", "accueil.html")
+    titre = "Page d’accueil"
+
+    if request.method == "POST":
+        nom = request.form.get("nom", "")
+        ville = request.form.get("ville", "")
+
+        return render_template(
+            "accueil.html",
+            titre=titre,
+            soumis=True,
+            nom=nom,
+            ville=ville
+        )
+
+    return render_template(
+        "accueil.html",
+        titre=titre,
+        soumis=False
+    )
 
 # Retourne la page correspondante dans static
-@app.route("/static/<name>")
+@app.route("/static/<name>", methods=["GET"])
 def static_html(name):
     filename = f"{name}.html"
     return send_from_directory("static", filename)
@@ -16,7 +34,7 @@ def static_html(name):
 # Gestion des erreurs 404
 @app.errorhandler(404)
 def page_not_found(error):
-    return send_from_directory("static", "erreur404.html"), 404
+    return render_template("erreur404.html"), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
